d013f9e09801491782b9ff4974554b3ba0841907
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-03-03 15:01:19 -0600

    mysql tables

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(100),
+    name         VARCHAR(100),
+    release_date INT,
+    sales        FLOAT,
+    genre        VARCHAR(20),
+    PRIMARY KEY (id)
+);
